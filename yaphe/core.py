4ac1d1b7bc9eb145effa014414e168ce1f4dfbed
John LeGrande <jowsterguy@yahoo.com>
2011-05-18 21:12:37 -0700

    Added helper module

--- yaphe/core.py
+++ yaphe/core.py
@@ -117,17 +117,3 @@ class Elem(object):
         
         return elem
         
-def elem(tag, content='', attrs={}, self_closing=False):
-    return Elem(tag, content, attrs, self_closing)
-
-def screen_stylsheet(href):
-    return Elem("link", attrs={'rel': 'stylesheet',
-                               'href': href,
-                               'type': 'text/css',
-                               'media': 'screen'})
-
-def img(src, alt='', attrs={}):
-    attrs['src'] = src
-    if alt:
-        attrs['alt'] = alt
-    return Elem("img", attrs=attrs) 
---
+++ yaphe/helpers.py
@@ -0,0 +1,16 @@
+from core import Elem
+
+def elem(tag, content='', attrs={}, self_closing=False):
+    return Elem(tag, content, attrs, self_closing)
+
+def screen_stylsheet(href):
+    return Elem("link", attrs={'rel': 'stylesheet',
+                               'href': href,
+                               'type': 'text/css',
+                               'media': 'screen'})
+
+def img(src, alt='', attrs={}):
+    attrs['src'] = src
+    if alt:
+        attrs['alt'] = alt
+    return Elem("img", attrs=attrs) 
